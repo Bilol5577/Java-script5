09b8152dc25bacfe06806cec21c2000dd25ebb69
Bilol5577 <bilolbektursunov079@gmail.com>
2024-11-16 18:42:02 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+let reegistor = {}
+
+reegistor.name = prompt('Ismingizni yozing');
+
+reegistor.id = prompt('Space idingizni kiriting');
+
+reegistor.password = prompt('Space passwordingizni kiriting');
+
+console.log(reegistor);
+
+// alert(reegistor);
